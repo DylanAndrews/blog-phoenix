22fe0becc840e97c8679bec7a4c234e4c948c3f1
Dylan Andrews <dylanandr@gmail.com>
2018-10-07 22:45:36 -0500

    Handle incorrect login

--- web/controllers/sessions.controller.ex
+++ web/controllers/sessions.controller.ex
@@ -1,5 +1,6 @@
 defmodule Phoenixblog.SessionController do
   import Comeonin.Bcrypt, only: [checkpw: 2]
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
   use Phoenixblog.Web, :controller
   alias Phoenixblog.User
 
@@ -9,9 +10,14 @@ defmodule Phoenixblog.SessionController do
     render conn, "new.html", changeset: User.changeset(%User{})
   end
 
-  def create(conn, %{"user" => user_params}) do
-    Repo.get_by(User, username: user_params["username"])
-    |> sign_in(user_params["password"], conn)
+  def create(conn, %{"user" => %{"username" => username, "password" => password}})
+    when not is_nil(username) and not is_nil(password) do
+      user = Repo.get_by(User, username: username)
+      sign_in(user, password, conn)
+  end
+
+  def create(conn, _) do
+    failed_login(conn)
   end
 
   def delete(conn, _params) do
@@ -21,10 +27,13 @@ defmodule Phoenixblog.SessionController do
     |> redirect(to: page_path(conn, :index))
   end
 
-  defp sign_in(user, password, conn) when is_nil(user) do
+  defp failed_login(conn) do
+    dummy_checkpw()
     conn
+    |> put_session(:current_user, nil)
     |> put_flash(:error, "Invalid username/password combination!")
     |> redirect(to: page_path(conn, :index))
+    |> halt()
   end
 
   defp sign_in(user, password, conn) do
@@ -34,10 +43,11 @@ defmodule Phoenixblog.SessionController do
       |> put_flash(:info, "Sign in successful!")
       |> redirect(to: page_path(conn, :index))
     else
-      conn
-      |> put_session(:current_user, nil)
-      |> put_flash(:error, "Invalid username/password combination!")
-      |> redirect(to: page_path(conn, :index))
+      failed_login(conn)
     end
+  end
+
+  defp sign_in(user, _password, conn) when is_nil(user) do
+    failed_login(conn)
   end
 end
