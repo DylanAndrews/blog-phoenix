48d66cb13946bfef475ac00219f1044e39ed8918
Dylan Andrews <dylanandr@gmail.com>
2018-10-20 17:42:27 -0500

    Add markdown support

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Phoenixblog.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:earmark, "~> 1.0.1"},
       {:phoenix, "~> 1.3.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -1,3 +1,8 @@
 defmodule Phoenixblog.PostView do
   use Phoenixblog.Web, :view
+  def markdown(body) do
+    body
+    |> Earmark.to_html
+    |> raw
+  end
 end
