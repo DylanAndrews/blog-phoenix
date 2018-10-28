7d9ed07294656646a73ae50f629757a7c8b955c7
Dylan Andrews <dylanandr@gmail.com>
2018-10-28 15:48:02 -0500

    Update post controllers and views and implement earmark for markdown Set up bootstrap

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule Phoenixblog.PageController do
   use Phoenixblog.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    posts = Repo.all(assoc(conn.assigns[:user], :posts))
+    render(conn, "index.html", posts: posts)
   end
 end
